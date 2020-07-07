4dfe8431e76eb809838a4050eec6d177ba536e79
ph0ph0 <phillipalanlee@googlemail.com>
2020-07-07 22:08:20 +0100

    finished grouping by date, started grouping by week

--- StatsByDates.py
+++ StatsByDates.py
@@ -14,10 +14,76 @@ print('Pandas Version: ' + pd.__version__)
 print('Matplotlib Version: ' + matplotlib.__version__)
 
 # Import the csv data
-Location = r"/Users/Pho/Documents/Freelance/Projects/GrahamStockTradingPlatform/Code/TradingScripts/StatsByDates/CSV_Data/test.csv"
+Location = r"CSV_Data/test.csv"
 
 # Create dataframe out of CSV data
 Headers = ['No', 'Time', 'Type', 'Order', 'Size',
            'Price', 'SL', 'TP', 'Profit', 'Balance']
 df = pd.read_csv(Location, names=Headers,  encoding="ISO-8859-1")
+
+# df.info(verbose=True)
+
+# Remove unnecessary columns
+del df["SL"]
+del df["TP"]
+del df["Balance"]
+del df["Size"]
+
+# Create arrays to hold extracted data
+OrderNumberArray = []
+OrderTypeArray = []
+OpenTimeArray = []
+CloseTimeArray = []
+OpenPriceArray = []
+ClosePriceArray = []
+ProfitArray = []
+
+# Extract data
+for row in df.itertuples():
+    if (row[0] % 2) == 0:
+        OrderNumberArray.append(row[4])
+        OrderTypeArray.append(row[3])
+        OpenTimeArray.append(row[2])
+        OpenPriceArray.append(row[5])
+    if (row[0] % 2) != 0:
+        CloseTimeArray.append(row[2])
+        ClosePriceArray.append(row[5])
+        ProfitArray.append(row[6])
+
+data = {"OrderNumber": OrderNumberArray, "OrderType": OrderTypeArray, "OpenTime": OpenTimeArray,
+        "CloseTime": CloseTimeArray, "OpenPrice": OpenPriceArray, "ClosePrice": ClosePriceArray, "Profit": ProfitArray}
+
+columnNames = ["OrderNumber", "OrderType", "OpenTime",
+               "CloseTime", "OpenPrice", "ClosePrice", "Profit"]
+
+# Overwrite existing dataframe
+df = pd.DataFrame(data, columns=columnNames)
+
+todaysDate = date.today().strftime("%Y_%m_%d")
+newCSVName = "ProcessedData/" + todaysDate + "_CleanedOrdersList"
+df.to_csv(newCSVName, index=False)
+
+# Convert OpenTime/CloseTime to datetime
+df["OpenTime"] = pd.to_datetime(df["OpenTime"], format="%Y.%m.%d %H:%M")
+df["CloseTime"] = pd.to_datetime(df["CloseTime"], format="%Y.%m.%d %H:%M")
+
+# Extract Open/CloseTime and convert to DayOfWeek
+OpenTimeDayArray = []
+CloseTimeDayArray = []
+for row in df.itertuples():
+    OpenTimeDayArray.append(row[3].dayofweek)
+    CloseTimeDayArray.append(row[4].dayofweek)
+
+df.insert(3, "OrderOpenDay", OpenTimeDayArray)
+df.insert(5, "OrderCloseDay", CloseTimeDayArray)
+
 print(df)
+
+# Groupby date
+groupedByDate = df.groupby(df["OpenTime"].dt.date)["Profit"].agg(
+    [np.ma.count, np.sum, np.mean, np.median, stats.mode, np.ptp, stats.iqr, np.std, "min", "max"])
+
+# Groupby week
+groupedByWeek = df["OpenTime"].resample('W')
+
+print(groupedByWeek)
